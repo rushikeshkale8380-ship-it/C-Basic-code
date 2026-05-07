438227ba66f8d089227ab034c1a2ff6debfcc342
Hrishikesh Satish kale <rushikeshkale8380@gmail.com>
2026-05-08 00:29:15 +0530

    Add files via upload
    They Are Auto storage class program all variables default they are auto storage class and they store in Ram at Stack Section

---
+++ autoStorageclass.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Below All Variables are auto Storage classes
+    int i;    
+    auto j=0;  
+    int k=0;
+
+    cout<<"They Are All Variable are auto storage class";
+
+    return 0;
+}
