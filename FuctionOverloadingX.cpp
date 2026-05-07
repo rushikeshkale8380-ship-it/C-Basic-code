6d729d5cbc94ce34c74c9231fba2eea644bc89de
Hrishikesh Satish kale <rushikeshkale8380@gmail.com>
2026-05-07 23:54:03 +0530

    Add files via upload
    It is Function overloading Concept they are all function names are same they are called Automatically That is the Polymorphism.

---
+++ FuctionOverloadingX.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class First    //It is Class Crearte
+{
+    public:
+    int Addition(int a,int b)   //Addition Fuction they are pass Two integer and return Sum 
+    {
+        return a+b;
+    }
+
+    int Addition(int a,int b,int c)  //Addition Funtion they are pass Three integer and return sum
+    {
+        return a+b+c;
+    }
+
+    int Addition(int a,int b,int c,int d)  //Addition Funtio they are pass Four integer and return sum
+    {
+        return a+b+c+d;
+    }
+};
+
+int main()
+{
+    First fobj;              //Create Object of First class
+    cout<<fobj.Addition(12,12)<<"\n";   //It is Call Addition function they returns Tow integer sum
+    cout<<fobj.Addition(11,22,33)<<"\n";  //It is Call Addition function they returns Three integer Sum
+    cout<<fobj.Addition(11,22,33,44)<<"\n";  //It is call Addition function they returns four integer sum
+
+    return 0;
+
+}
