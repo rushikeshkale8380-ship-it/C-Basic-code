dc12a89a8cfada7cfae6ccdc8c3903df70da462a
Hrishikesh Satish kale <rushikeshkale8380@gmail.com>
2026-05-13 17:11:43 +0530

    Add files via upload
    It is Code About the Access Specifire in c++ they are three types of Access Specifire Private,Protected,Public.

---
+++ AccessSpecifire.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public:
+    int i;
+    private:
+    int j;
+    protected:
+    int k;
+    
+    public:
+    Demo()
+    {
+        i=0;
+        j=0;
+        k=0;
+    }
+
+    void Display()
+    {
+        cout<<"Value of i:"<<i<<"\n";
+        cout<<"Value of j:"<<j<<"\n";
+        cout<<"Value of k:"<<k<<"\n";
+    }
+};
+
+int main()
+{
+    Demo dobj;
+    dobj.Display();
+
+    cout<<"Value of i:"<<dobj.i<<"\n";
+    cout<<"Value of j:"<<dobj.j<<"\n";   //Not Allowed (it is private not Accesable outside the class)
+    cout<<"Value of k:"<<dobj.k<<"\n";   //Not Allowed (it is protected not Accessable outside the class)
+
+    return 0;
+}
+
